852a02968cb4f4b423dae506651b072b29a1c674
Wilson Muh <wilson.muh@gmail.com>
2016-01-21 16:45:47 -0800

    added paperclip for image uploads

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'coffee-rails', '~> 4.1.0'
 # gem 'therubyracer', platforms: :ruby
 gem 'bootstrap-sass'
 gem 'devise'
+gem 'paperclip', github: 'thoughtbot/paperclip'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   # GET /pins
   # GET /pins.json
@@ -68,8 +69,14 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
+    def correct_user      
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Nacho pin!" if @pin.nil?
+      
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
