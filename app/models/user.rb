bae9498cbc07e21ef07be4366e082bf706e5a43d
Wilson Muh <wilson.muh@gmail.com>
2016-01-22 02:57:26 -0800

    requires name from user and image/description from pin

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :name, presence: true, length: {maximum: 75}
+
 
   
 end
